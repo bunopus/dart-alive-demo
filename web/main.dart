e04e8afcd87bd6e6b95c5f275192c727d3842670
Evgeny Gusev <bunopus@gmail.com>
2017-06-18 20:09:30 +0300

    Ninth Step: SDK: streams

--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,15 @@
 // Copyright (c) 2017, kestrel. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-void main() {
-  Iterable<int> test = [1,2,3];
-  var res = test
-      .where((i) => i%2 != 0)
-      .map((i) => i*10)
-      .toList();
-  print(res);
+import 'dart:async';
+
+main() async {
+  var stream = countStream(10);
+  stream.listen((i) => print(i));
+}
+
+Stream<int> countStream(int to) async* {
+  for (int i = 1; i <= to; i++) {
+    yield i;
+  }
 }
