51d2c27621886eecc15c5d940152661dc25dfaaa
Evgeny Gusev <bunopus@gmail.com>
2017-06-18 20:57:24 +0300

    Tenth Step: js interop

--- web/main.dart
+++ web/main.dart
@@ -1,15 +1,10 @@
 // Copyright (c) 2017, kestrel. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
+import 'dart:js';
+import 'peasant.dart';
 
 main() async {
-  var stream = countStream(10);
-  stream.listen((i) => print(i));
-}
-
-Stream<int> countStream(int to) async* {
-  for (int i = 1; i <= to; i++) {
-    yield i;
-  }
+  var peasant = new Peasant();
+  peasant.doSomething(allowInterop(() => print('Work! Ya blody bastard!')));
 }
---
+++ web/peasant.dart
@@ -0,0 +1,10 @@
+@JS()
+library peasant;
+
+import 'package:js/js.dart';
+
+@JS()
+class Peasant {
+  external Peasant();
+  external void doSomething(Function action);
+}
