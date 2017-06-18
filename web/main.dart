a5bc8ec7e7fa1eacea858899b661318db204cee1
Evgeny Gusev <bunopus@gmail.com>
2017-06-18 20:03:19 +0300

    Eight Step: SDK: iterables

--- web/main.dart
+++ web/main.dart
@@ -1,13 +1,11 @@
 // Copyright (c) 2017, kestrel. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-
-
 void main() {
-  waitForIt().then((_) => print('Hello!'));
-}
-
-Future waitForIt() {
-  return new Future.delayed(new Duration(milliseconds: 100));
+  Iterable<int> test = [1,2,3];
+  var res = test
+      .where((i) => i%2 != 0)
+      .map((i) => i*10)
+      .toList();
+  print(res);
 }
